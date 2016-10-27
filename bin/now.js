fb1bf87e0e6e9fb50be302d0155a999340f2adb3
codyzu <cody.zuschlag@gmail.com>
2016-10-27 13:02:43 +0200

    refactor parsing

--- bin/now.js
+++ bin/now.js
@@ -56,57 +56,27 @@ const aliases = new Map([
   ['secret', 'secrets']
 ])
 
-let args = process.argv.slice(2)
 let cmd = defaultCommand
+let args = process.argv.slice(2)
+let index = args.findIndex((arg) => commands.has(arg))
 
-let cmdIndex
-console.log('HELLO')
-for (const [index, arg] of args.entries()) {
-  console.log('TESTING:', arg)
-  if (commands.has(arg)) {
-    console.log('CMD FOUND:', arg, 'INDEX:', index)
-    cmd = arg
-    args.splice(index, 1)
+if (index > -1) {
+  cmd = args[index]
+  args.splice(index, 1)
 
-    if (arg === 'help') {
-      if (index + 1 < args.length && commands.has(args[index + 1])) {
-        cmd = args[index + 1]
-        args.splice(index, 2)
-      } else {
-        cmd = defaultCommand
-        args.splice(index, 1)
-      }
-
-      args.unshift('--help')
-    } else {
-      cmd = arg
+  if (cmd === 'help') {
+    if (index < args.length && commands.has(args[index])) {
+      cmd = args[index]
       args.splice(index, 1)
+    } else {
+      cmd = defaultCommand
     }
 
-    break
+    args.unshift('--help')
   }
-}
 
-// let cmd = argv._[0]
-// let args = []
-//
-// if (cmd === 'help') {
-//   cmd = argv._[1]
-//
-//   if (!commands.has(cmd)) {
-//     cmd = defaultCommand
-//   }
-//
-//   args.push('--help')
-// }
-//
-// if (commands.has(cmd)) {
-//   cmd = aliases.get(cmd) || cmd
-//   args = args.concat(process.argv.slice(3))
-// } else {
-//   cmd = defaultCommand
-//   args = args.concat(process.argv.slice(2))
-// }
+  cmd = aliases.get(cmd) || cmd
+}
 
 let bin = resolve(__dirname, 'now-' + cmd)
 if (process.pkg) {
@@ -114,14 +84,12 @@ if (process.pkg) {
   bin = process.execPath
 }
 
-console.log('CMD:', bin)
-console.log('ARGS:', args)
-//
-//
-// const proc = spawn(bin, args, {
-//   stdio: 'inherit',
-//   customFds: [0, 1, 2]
-// })
-//
-// proc.on('close', code => exit(code))
-// proc.on('error', () => exit(1))
+console.log('CMD:', bin, 'ARGS:', args)
+
+const proc = spawn(bin, args, {
+  stdio: 'inherit',
+  customFds: [0, 1, 2]
+})
+
+proc.on('close', code => exit(code))
+proc.on('error', () => exit(1))
