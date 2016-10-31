73c6978191c331e47a099a97536ba0fdff15a0df
codyzu <cody.zuschlag@gmail.com>
2016-10-31 13:09:07 +0100

    rework test

--- test/console.js
+++ test/console.js
@@ -1,6 +1,33 @@
 import test from 'ava'
 import {spawn} from 'cross-spawn'
+import {join} from 'path'
 
-test('dsafdf', t => {
+test.cb('dsafdf', t => {
+  // t.plan(1)
+  // const command = path.join(__dirname, '../bin/now')
+  // const command = 'pwd'
+  const command = 'node'
+  const args = ['-r', 'babel-register', '../bin/now', '--help']
+  // const args = ['--help']
+  const now = spawn(command, args)
 
+  now.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  now.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  now.on('error', (err) => {
+    console.log(`error: ${err}`)
+    t.fail(err)
+    // console.log(`stderr: ${data}`);
+  });
+
+  now.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    t.pass();
+    t.end()
+  });
 })
